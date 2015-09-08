6c3d590e85540451a8d8efcc0eac0171eb428c65
ipalm0423 <76423ok@gmail.com>
2015-09-08 15:46:33 +0800

    Initial Commit

---
+++ Polygrath/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Polygrath
+//
+//  Created by 陳冠宇 on 2015/9/8.
+//  Copyright © 2015年 陳冠宇. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
